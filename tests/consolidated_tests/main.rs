65e006478b4178e259826df6fdca4d7ffff738ba
Bennet Bleßmann <bennet.blessmann@stu.uni-kiel.de>
2021-04-01 21:28:29 +0200

    add a test where I think an incorrect result is produced

--- tests/consolidated_tests/main.rs
+++ tests/consolidated_tests/main.rs
@@ -4,6 +4,60 @@ mod server_tests;
 mod system_tests;
 mod task_tests;
 mod window_tests;
+
+mod incorrect {
+    use crate::broken_assumption::curve_has_no_non_trivial_overlap;
+    use rta_for_fps::iterators::CurveIterator;
+    use rta_for_fps::server::{Server, ServerKind};
+    use rta_for_fps::system::System;
+    use rta_for_fps::task::Task;
+
+    #[test]
+    fn incorrect() {
+        let servers = &[
+            Server {
+                tasks: &[Task::new(8, 32, 8)],
+                capacity: 8.into(),
+                interval: 16.into(),
+                server_type: ServerKind::Deferrable,
+            },
+            Server {
+                tasks: &[Task::new(4, 16, 8)],
+                capacity: 4.into(),
+                interval: 16.into(),
+                server_type: ServerKind::Deferrable,
+            },
+            Server {
+                tasks: &[Task::new(2, 16, 0)],
+                capacity: 2.into(),
+                interval: 16.into(),
+                server_type: ServerKind::Deferrable,
+            },
+        ];
+
+        let system = System::new(servers);
+
+        let swh1 = system.system_wide_hyper_periode(1);
+        let swh2 = system.system_wide_hyper_periode(2);
+        let aes1 = system.actual_execution_curve_iter(1, swh1);
+        let aes2 = system.actual_execution_curve_iter(2, swh2);
+
+        let aes1c = aes1.collect_curve();
+        let aes2c = aes2.collect_curve();
+
+        eprintln!("{:#?}\n\n{:#?}", aes1c, aes2c);
+
+        let result = curve_has_no_non_trivial_overlap(&aes1c, &aes2c);
+        assert!(result);
+
+        let wcrt1 = Task::worst_case_response_time(&system, 1, 0, swh1);
+        assert_eq!(wcrt1, 12.into());
+
+        let wcrt2 = Task::worst_case_response_time(&system, 2, 0, swh2);
+        assert_eq!(wcrt2, 6.into());
+    }
+}
+
 mod util {
     use rta_for_fps::curve::curve_types::CurveType;
     use rta_for_fps::curve::Curve;
@@ -18,13 +72,13 @@ mod util {
         if !expected.eq_curve_iterator(result.clone()) {
             panic!(
                 "\
-            Curves did not match:\
-            Expected:\
-            {:#?}\
-            
-            Got:\
-            {:#?}\
-            \
+            Curves did not match:\n\
+            Expected:\n\
+            {:#?}\n\
+            \n\
+            Got:\n\
+            {:#?}\n\
+            \n\
             ",
                 expected,
                 result.collect_curve::<Curve<_>>()
